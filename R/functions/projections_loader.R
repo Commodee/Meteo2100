ccc15735b23e7fb1c7f73961e9fa1a8b8b7b15c3
Victor <victor.frison@outlook.com>
2025-12-07 19:43:56 +0100

    unified les downloads

--- R/functions/projections_loader.R
+++ R/functions/projections_loader.R
@@ -1,6 +1,6 @@
-load_drias_projections <- function(chemin_fichier = "../data/prevision_DRIAS.txt") {
+process_drias_projections <- function(chemin_fichier) {
   raw_data <- read.csv2(chemin_fichier, sep = ";", header = TRUE, dec=".")
-
+  
   full_data <- raw_data %>%
     mutate(
       annee = case_when(
@@ -8,6 +8,7 @@ load_drias_projections <- function(chemin_fichier = "../data/prevision_DRIAS.txt
         Periode == "H1" ~ 2025,
         Periode == "H2" ~ 2055,
         Periode == "H3" ~ 2085,
+        TRUE ~ as.numeric(as.character(Periode)) # Sécurité si jamais
       ),
       
       Contexte = case_when(
@@ -27,11 +28,5 @@ load_drias_projections <- function(chemin_fichier = "../data/prevision_DRIAS.txt
       .groups = "drop"
     )
   
-
-
-  
   return(summarise_data)
 }
-
-
-
